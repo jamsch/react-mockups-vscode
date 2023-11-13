9b5ce0b3f0a4389384dcb50f12fbb4ac84967b47
james-libros <james@librosmedia.com>
2023-11-13 13:15:11 +1300

    update server

--- src/createWebsocketServer.ts
+++ src/createWebsocketServer.ts
@@ -1,58 +1,37 @@
 import { WebSocket, WebSocketServer } from "ws";
-import { log } from "./logger";
 
-type Mockups = Array<{
-  title: string;
-  path: string;
-  children?: Mockups;
-}>;
-
-type AppState = {
-  /** The project's root directory. Useful for navigating to individual mockup files */
-  projectRoot: string;
-  /** Whether the server has synced with an app client */
-  hasSynced: boolean;
-  /** Current relative path to mockup. You can determine the active mockup by searching "mockups[x].path" */
-  path: string | null;
-  /** List of synced mockups with the app client */
-  mockups: Mockups[];
-};
-
-export function createWebsocketServer(
-  hostname = "127.0.0.1",
-  port = 1337,
-  onReady?: (params: { broadcast(type: string, payload: any): void }) => void
-) {
+export function createWebsocketServer(hostname = "127.0.0.1", port = 1337) {
   const wss = new WebSocketServer({ path: "/websocket", port, host: hostname });
 
   const state = {
     clients: [] as WebSocket[],
-    app: {
-      projectRoot: process.cwd(),
-      path: "",
-      hasSynced: false,
-      mockups: [],
-    } as AppState,
+    activeMockup: null as null | {
+      path: string;
+      exportName: string;
+    },
   };
 
   const broadcast = (type: string, payload: any) => {
+    if (type === "VIEW") {
+      state.activeMockup = payload;
+    }
     for (const client of state.clients) {
       client.send(JSON.stringify({ type, payload }));
     }
   };
 
   wss.on("connection", (ws) => {
-    onReady?.({ broadcast });
+    if (state.activeMockup) {
+      ws.send(JSON.stringify({ type: "VIEW", payload: state.activeMockup }));
+    }
     state.clients.push(ws);
 
     // Remove the client from the list of connected clients
     ws.on("close", () => {
-      console.log("closed", ws);
       state.clients = state.clients.filter((client) => client !== ws);
     });
 
     ws.on("message", (data, isBinary) => {
-      // WS 8 breaking change: messages can be binary
       const message = isBinary ? data : data.toString();
 
       if (typeof message !== "string") {
@@ -61,39 +40,9 @@ export function createWebsocketServer(
 
       const messageData = JSON.parse(message);
 
-      console.log("message", messageData);
-
       switch (messageData.type) {
-        // When IDE clients try to connect to the server, send back app state
-        case "PING": {
-          if (state.app.hasSynced) {
-            ws.send(JSON.stringify({ type: "SYNC_STATE", payload: state.app }));
-          }
-          break;
-        }
-        // When the app client connects, store the current state & update IDE clients
-        case "UPDATE_STATE": {
-          state.app = {
-            ...state.app, // retain "projectRoot"
-            ...(messageData.payload as AppState),
-            hasSynced: true,
-          };
-          // Update clients
-          for (const client of state.clients.filter((c) => c !== ws)) {
-            client.send(
-              JSON.stringify({ type: "SYNC_STATE", payload: state.app })
-            );
-          }
-          break;
-        }
-        case "NAVIGATE": {
-          state.app.path = messageData.payload as string;
-          // Update both IDE & app clients (except the one that sent the message)
-          for (const client of state.clients.filter((c) => c !== ws)) {
-            client.send(
-              JSON.stringify({ type: "NAVIGATE", payload: state.app.path })
-            );
-          }
+        case "CLOSE": {
+          state.activeMockup = null;
           break;
         }
       }
@@ -101,6 +50,7 @@ export function createWebsocketServer(
   });
 
   return {
+    state,
     broadcast,
   };
 }
--- src/extension.ts
+++ src/extension.ts
@@ -3,32 +3,13 @@
 import * as vscode from "vscode";
 import { MockupCodeLensProvider } from "./MockupCodeLensProvider";
 import { createWebsocketServer } from "./createWebsocketServer";
+import { log } from "./logger";
 
 let websocketServer: ReturnType<typeof createWebsocketServer> | null = null;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "react-mockups-vscode" is now active!'
-  );
-
-  // Start mockup server
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "react-mockups-vscode.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from react-mockups!");
-    }
-  );
-
   vscode.commands.registerCommand(
     "react-mockups.openMockup",
     (args: { mockup: { fullPath: string; exportName: string } }) => {
@@ -42,29 +23,21 @@ export function activate(context: vscode.ExtensionContext) {
         "."
       );
 
-      console.log("VIEW", {
+      log.appendLine(
+        "[openMockup]: " +
+          JSON.stringify({
+            path: relativePath,
+            exportName: args.mockup.exportName,
+          })
+      );
+
+      if (!websocketServer) {
+        websocketServer = createWebsocketServer("127.0.0.1", 1337);
+      }
+      websocketServer.broadcast("VIEW", {
         path: relativePath,
         exportName: args.mockup.exportName,
       });
-
-      if (!websocketServer) {
-        console.log("creating server...");
-        websocketServer = createWebsocketServer(
-          "127.0.0.1",
-          1337,
-          ({ broadcast }) => {
-            broadcast("VIEW", {
-              path: relativePath,
-              exportName: args.mockup.exportName,
-            });
-          }
-        );
-      } else {
-        websocketServer.broadcast("VIEW", {
-          path: relativePath,
-          exportName: args.mockup.exportName,
-        });
-      }
     }
   );
 
@@ -79,7 +52,7 @@ export function activate(context: vscode.ExtensionContext) {
     new MockupCodeLensProvider()
   );
 
-  context.subscriptions.push(disposable, codeLensProviderDisposable);
+  context.subscriptions.push(codeLensProviderDisposable);
 }
 
 // This method is called when your extension is deactivated
